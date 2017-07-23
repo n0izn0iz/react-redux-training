0326cdf6451712e9e1f53fdd7e64d97cd59c7e7e
n0iz <n0iz@nexus.localdomain>
2017-07-23 22:39:58 +0200

    fix and refactor PostForm

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,38 +1,65 @@
 import React from 'react';
-import FormElem from './FormElem';
+
+const PostForm = (props) => {
+  return (
+    <form onSubmit={ bindSubmit(props.createPost, props.post) }>
+      <h3>Create new post</h3>
+      <span className="label label-warning">Due to the nature of JSONPlaceholder, you are limited to one new post, subsequent new posts will replace the existing one</span>
+      <Title
+        value={ props.post.title }
+        onChange={ bindValueGetter(props.changeTitle) }
+      />
+      <Body
+        value={ props.post.body }
+        onChange={ bindValueGetter(props.changeBody) }
+      />
+      <input className="btn btn-default" type="submit" />
+    </form>
+  );
+};
+
+const Title = (props) => {
+  return (
+    <div className="form-group">
+      <label>
+        Title
+        <input
+          className="form-control"
+          value={ props.value }
+          onChange={ props.onChange }
+          type="text"
+        />
+      </label>
+    </div>
+  );
+};
+
+const Body = (props) => {
+  return (
+    <div className="form-group">
+      <label>
+        Title
+        <textarea
+          className="form-control"
+          value={ props.value }
+          onChange={ props.onChange }
+        />
+      </label>
+    </div>
+  );
+};
+
+const bindSubmit = (createPost, post) => {
+  return (event) => {
+    event.preventDefault();
+    createPost(post);
+  };
+};
 
 const bindValueGetter = (callback) => {
   return (event) => {
     callback(event.target.value)
-  }
-}
-
-class PostForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-  }
-  submit(event) {
-    event.preventDefault();
-    this.props.createPost(this.props.post);
-  }
-  render() {
-    return (
-      <form onSubmit={ this.submit }>
-        <h3>Create new post</h3>
-        <span className="label label-warning">Due to the nature of JSONPlaceholder, you are limited to one new post, subsequent new posts will replace the existing one</span>
-        <FormElem
-          value={ this.props.post.title }
-          onChange={ bindValueGetter(this.props.changeTitle) }
-        />
-        <FormElem
-          value={ this.props.post.body }
-          onChange={ bindValueGetter(this.props.changeBody) }
-        />
-        <input className="btn btn-default" type="submit" />
-      </form>
-    );
-  }
-}
+  };
+};
 
 export default PostForm;
